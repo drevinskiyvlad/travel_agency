026f2d2286fa563e545327ca98bcc76966a30216
Greatest person <drevinskiy.vlad@gmail.com>
2023-01-16 22:29:11 +0200

    removed logger

--- src/main/java/com/travel_agency/exceptions/DAOException.java
+++ src/main/java/com/travel_agency/exceptions/DAOException.java
@@ -1,8 +1,5 @@
 package com.travel_agency.exceptions;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.sql.SQLException;
 
 public class DAOException extends SQLException {
--- src/main/java/com/travel_agency/exceptions/ValidationException.java
+++ src/main/java/com/travel_agency/exceptions/ValidationException.java
@@ -1,8 +1,5 @@
 package com.travel_agency.exceptions;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
