1f522b7cd116eb951b0bf6a8cec9eaad14eddbf9
Greatest person <drevinskiy.vlad@gmail.com>
2023-01-22 22:18:14 +0200

    added logger to DateTagHandler

--- src/main/java/com/travel_agency/controller/tag/DateTagHandler.java
+++ src/main/java/com/travel_agency/controller/tag/DateTagHandler.java
@@ -1,29 +1,28 @@
 package com.travel_agency.controller.tag;
 
-import jakarta.servlet.jsp.JspException;
 import jakarta.servlet.jsp.JspWriter;
 import jakarta.servlet.jsp.tagext.TagSupport;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
 
 public class DateTagHandler extends TagSupport {
-
+    private static final Logger logger = LogManager.getLogger(DateTagHandler.class);
     @Override
-    public int doStartTag() throws JspException {
+    public int doStartTag() {
         JspWriter out=pageContext.getOut();//returns the instance of JspWriter
         try{
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
             out.print(dtf.format(LocalDateTime.now()));//printing date and time using JspWriter
-        }catch(Exception e){System.out.println(e);}
+        }catch(Exception e){
+            logger.error("Error while handle tag",e);
+        }
         return SKIP_BODY;//will not evaluate the body content of the tag
     }
     @Override
-    public int doEndTag() throws JspException {
+    public int doEndTag() {
         return EVAL_PAGE;
     }
 
