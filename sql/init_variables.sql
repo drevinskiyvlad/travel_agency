927d3fb89e650e19dd16c4c612cb4c33c47d8680
Greatest person <drevinskiy.vlad@gmail.com>
2023-01-29 16:50:57 +0200

    added offers to init parameters

--- sql/init_variables.sql
+++ sql/init_variables.sql
@@ -16,3 +16,18 @@ INSERT INTO offer_type VALUES(3,'shopping');
 INSERT INTO user_role VALUES(1,'user');
 INSERT INTO user_role VALUES(2,'manager');
 INSERT INTO user_role VALUES(3,'admin');
+
+INSERT INTO offer VALUES ('OF1', 'Bucharest',1,1,'Bucharest Grand Hotel',100,0.1,true,100.0);
+INSERT INTO offer VALUES ('OF2', 'Budapest',2,2,'Budapest Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF3', 'Cairo',3,3,'Cairo Grand Hotel',100,0.1,true,100.0);
+INSERT INTO offer VALUES ('OF4', 'Canberra',1,4,'Canberra Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF5', 'Doha',2,5,'Doha Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF6', 'Istanbul',3,6,'Istanbul Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF7', 'Kyiv',1,1,'Kyiv Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF8', 'London',2,2,'London Grand Hotel',100,0.1,true,100.0);
+INSERT INTO offer VALUES ('OF9', 'Male',3,3,'Male Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF10', 'NewYork',1,4,'NewYork Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF11', 'Paris',2,5,'Paris Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF12', 'Rijeka',3,6,'Rijeka Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF13', 'Zagreb',1,1,'Zagreb Grand Hotel',100,0.1,false,100.0);
+INSERT INTO offer VALUES ('OF14', 'Kyiv',2,2,'Kyiv Grand Hotel',100,0.1,false,100.0);
