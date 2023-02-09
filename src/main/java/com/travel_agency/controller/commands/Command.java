71daac39072bfc73321834c2820397a62003238d
Greatest person <drevinskiy.vlad@gmail.com>
2023-02-09 12:01:42 +0200

    added javadoc

--- src/main/java/com/travel_agency/controller/commands/Command.java
+++ src/main/java/com/travel_agency/controller/commands/Command.java
@@ -5,6 +5,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+/**
+ * Main interface for the Command pattern implementation.
+ *
+ * @author Drevinskyi Vladislav
+ */
 public interface Command {
+    /**
+     * Execution method for command.
+     *
+     * @return Address to go once the command is executed.
+     */
     String execute(HttpServletRequest req, HttpServletResponse resp) throws IOException;
 }
